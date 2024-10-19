a5cc07436ef0fc53178eb64d528bde518870d4d1
Shayan Ahmad <shayanahmad235@gmail.com>
2024-10-20 02:33:10 +0500

    create routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ERDController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ERDController::class, 'showForm']);
+Route::post('/generate-erd', [ERDController::class, 'generateERD']);
